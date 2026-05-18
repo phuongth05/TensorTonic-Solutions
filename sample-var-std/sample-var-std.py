4a871dbfd4129636fddb96b3ae43ef4fea2b5f6c
Trần Hồng Phương <163630215+phuongth05@users.noreply.github.com>
2026-05-18 23:53:09 +0700

    Update solution for sample-var-std

--- sample-var-std/sample-var-std.py
+++ sample-var-std/sample-var-std.py
@@ -5,10 +5,13 @@ def sample_var_std(x):
     Compute sample variance and standard deviation.
     """
     # Write code here
-    x = np.array(x)
-    _mean = np.mean(x)
 
-    var = 1/(x.shape[0]-1) * np.sum((x - _mean)**2)
-    std = np.sqrt(var)
-    return var, std
+    x = np.array(x, dtype=float)
+    n = x.shape[0]
+
+    _mean = np.mean(x)
+    _var = 1/(n-1) * np.sum((x - _mean)**2)
+    _std = np.sqrt(_var)
+
+    return (_var, _std)
     pass
