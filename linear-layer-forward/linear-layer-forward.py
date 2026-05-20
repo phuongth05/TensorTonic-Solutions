da34631ad1d71a82366009ef1f6db7c935ce7cee
Trần Hồng Phương <163630215+phuongth05@users.noreply.github.com>
2026-05-20 22:58:14 +0700

    Update solution for linear-layer-forward

--- linear-layer-forward/linear-layer-forward.py
+++ linear-layer-forward/linear-layer-forward.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 def linear_layer_forward(X, W, b):
     """
     Compute the forward pass of a linear (fully connected) layer.
@@ -9,18 +10,7 @@ def linear_layer_forward(X, W, b):
     W = np.array(W)
     b = np.array(b)
 
-    n = X.shape[0]
-    d_in = W.shape[0]
-    d_out = W.shape[1]
-    
-    Y = np.zeros((n, d_out), dtype=float)
-
-    for i in range(n):
-        for j in range(d_out):
-            _sum = 0
-            for k in range(d_in):
-                _sum += X[i, k] * W[k, j]
-
-            Y[i, j] = _sum + b[j]
+    Y = X @ W + b
 
     return Y.tolist()
+    
