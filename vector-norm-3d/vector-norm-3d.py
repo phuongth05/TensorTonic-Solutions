e1f5d49e64f991672b8e5c80ecfb929ffa9b335c
Trần Hồng Phương <163630215+phuongth05@users.noreply.github.com>
2026-05-22 13:29:40 +0700

    Add solution for vector-norm-3d

---
+++ vector-norm-3d/vector-norm-3d.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def vector_norm_3d(v):
+    """
+    Compute the Euclidean norm of 3D vector(s).
+    """
+    # Your code here
+    v = np.asarray(v)
+
+    if v.ndim == 1:
+        return np.sqrt(np.sum(v**2))
+    else: 
+        return np.sqrt(np.sum(v**2, axis=1))
+    pass
